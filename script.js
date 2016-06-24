7de3d050d7d6e49d9adce165b685e8f078ee5fa7
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-06-24 14:45:03 -0500

    created and populated object

---
+++ script.js
@@ -0,0 +1,16 @@
+var team = {
+  name: 'Real Madrid C.F.',
+  nickname: 'Los Blancos',
+  sport: 'Socccer',
+  realSportName: 'Futbol(missing the accent)',
+  country: 'Spain',
+  city: 'Madrid'
+  league: 'La Liga',
+  established: '1902 as Madrid Futbol Club',
+  numberOfPlayers: 24,
+  manager: 'Zinedine Zedan',
+  stadium: 'Estadio Bernabeau',
+  leagueChampions: 32,
+  copaDelReyChampions: 19,
+  championsLeagueChampions: 11
+}
