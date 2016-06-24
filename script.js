81405cbd7560a219c2146392e84119db0e241171
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-06-24 14:56:45 -0500

    added a few more object keys and defined them

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ var team = {
   sport: 'Socccer',
   realSportName: 'Futbol(missing the accent)',
   country: 'Spain',
-  city: 'Madrid'
+  city: 'Madrid',
   league: 'La Liga',
   established: '1902 as Madrid Futbol Club',
   numberOfPlayers: 24,
+  president: 'Florentine Perez',
   manager: 'Zinedine Zedan',
   stadium: 'Estadio Bernabeau',
   leagueChampions: 32,
   copaDelReyChampions: 19,
-  championsLeagueChampions: 11
+  championsLeagueChampions: 11,
+  
 }
