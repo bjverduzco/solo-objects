589d602f8e28aa0d90226ffe06499a84882343eb
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-07-11 08:04:02 -0500

    added the solo files

--- script.js
+++ script.js
@@ -14,5 +14,7 @@ var team = {
   leagueChampions: 32,
   copaDelReyChampions: 19,
   championsLeagueChampions: 11,
-  
+
 }
+
+console.log("My favorite team is " + team.name + ", their nick name is  \"" + team.nickname + "\". Of course they are a " + team.sport + "team, that plays in " + team.city + ", " + team.country + ". " + "They play at the top level in " + team.country + ", the league is called " + team.league + ". The club was established in " + team.established + ". The president is " + team.president + ", while the manager is " + team.manager + ". They play in the " + team.stadium ". They have a storied history with " + team.leagueChampions + "league titles.");
